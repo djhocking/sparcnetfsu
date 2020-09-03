c65aeac128d04dc2c731eb8aa8d5a97c33182dae
djhocking <djhocking@gmail.com>
2020-09-03 17:47:16 -0400

    fixed data issue and started processing

--- R/process_scr.R
+++ R/process_scr.R
@@ -1,50 +1,19 @@
+
+
+#---------- Setup ----------
+# Load packages
 library(dplyr)
 library(ggplot2)
 library(readr)
 library(lubridate)
 library(here)
+library(tidyr)
 library(oSCR)
 
+#read data
 sally <- read_csv(here("analysis", "data", "raw_data", "salamander.csv"))
 
-sally <- sally %>%
-  dplyr::mutate(date = mdy(date)) %>%
-  dplyr::arrange(plot, date, board)
-
-ggplot(filter(sally, species == "PCIN"), aes(svl, mass)) + geom_point(alpha = 0.3, color = "gray30") + theme_bw()
-
-# make ss and days since last primary period
-for(i in 1:length(unique(sally$plot))) {
-
-}
-
-pcin_1 <- sally %>%
-  dplyr::filter(species == "PCIN",
-                plot == 1,
-                !is.na(id),
-                !(id %in% c("<NA>"))) %>%
-  # tidyr::unite("id", c(sex, morph, markf), remove = FALSE) %>%
-  dplyr::select(pp, id = id, ss, board, sex) %>%
-  filter(sex %in% c("M", "F")) %>%
-  dplyr::mutate(id = as.factor(id),
-                sex = as.factor(if_else(sex %in% c("M", "F"), sex, "U"))) %>% # oSCR can only handle 2 sexes
-  as.data.frame()
-
-# ?data2oscr
-
-# how many occasions (ss) per session (pp)
-sess_obs <- sally %>%
-  group_by(pp) %>%
-  dplyr::select(pp, ss) %>%
-  summarise(K = max(ss, na.rm = TRUE)) %>%
-  arrange(pp)
-
-# sess_obs_1 <- pcin_1 %>%
-#   group_by(pp) %>%
-#   dplyr::select(pp, ss) %>%
-#   summarise(K = max(ss, na.rm = TRUE)) %>%
-#   arrange(pp)
-
+# Set board grid
 boards <- expand.grid(col = LETTERS[1:5],
                       row = 0:9,
                       stringsAsFactors = FALSE) %>%
@@ -53,8 +22,109 @@ boards <- expand.grid(col = LETTERS[1:5],
   mutate(X = rep(0:4, each = 10),
          Y = rep(0:9, 5))
 
-pcin_1 <- pcin_1 %>%
-  filter(sex %in% c("M", "F"))
+#--------- Clean and Prep Data ---------
+sally <- sally %>%
+  dplyr::mutate(date = mdy(date),
+                doy = yday(date),
+                doy_s = (doy - 182.5) / 182.5,
+                doy_s_2 = doy_s ^ 2) %>%
+  dplyr::arrange(plot, date, board)
+
+ggplot(filter(sally, species == "PCIN"), aes(svl, mass)) + geom_point(alpha = 0.3, color = "gray30") + theme_bw()
+
+# make ss and days since last primary period
+for(i in 1:length(unique(sally$plot))) {
+# not needed yet
+}
+
+# pull out just data for 1 plot with valid ids
+pcin_1 <- sally %>%
+  dplyr::filter(species == "PCIN",
+                plot == 5,
+                !is.na(id),
+                !(id %in% c("<NA>"))) %>%
+  # tidyr::unite("id", c(sex, morph, markf), remove = FALSE) %>%
+  dplyr::select(pp, id, ss, board, sex) %>%
+  filter(sex %in% c("M", "F")) %>%
+  dplyr::mutate(id = as.factor(id),
+                sex = as.factor(if_else(sex %in% c("M", "F"), sex, "U"))) %>% # oSCR can only handle 2 sexes
+  as.data.frame()
+
+# construct tdf
+doy <- pcin_1 %>%
+  group_by(pp) %>%
+  dplyr::select(pp, ss, doy_s, day_s_2)
+
+#--------- Check data ----------
+summary(pcin_1)
+
+# check that boards properly recorded
+unique(pcin_1$board)
+
+# Check for duplicate IDs
+counts <- pcin_1 %>%
+  # select(-Row, -flag) %>%
+  filter(!is.na(id)) %>%
+  # distinct() %>%
+  group_by(id, pp, ss) %>%
+  select(id, pp, ss) %>%
+  summarise(count = n()) %>%
+  filter(count > 1)
+
+counts
+
+pcin_wide <- pcin_1 %>%
+  # select(-Row, -flag) %>%
+  filter(!is.na(id)) %>%
+  # distinct() %>%
+  group_by(id, pp, ss) %>%
+  select(id, pp, ss) %>%
+  summarise(count = n()) %>%
+  ungroup() %>%
+  group_by(id) %>%
+  tidyr::pivot_wider(names_from = c("pp", "ss"), values_from = count, names_sort = TRUE, names_prefix = "x")
+pcin_wide
+
+# more duplicate checking
+wide_summary <- pcin_wide %>%
+  ungroup() %>%
+  mutate(max = apply(select(., starts_with("x")), 1, max, na.rm = TRUE),
+         sum = rowSums(select(., starts_with("x")), na.rm = TRUE)) %>%
+  dplyr::select(id, max, sum) %>%
+  as.data.frame()
+
+wide_summary
+summary(wide_summary)
+
+# check if any sessions with no captures
+pcin_wide %>%
+  ungroup() %>%
+  dplyr::select(-id) %>%
+  colSums(. , na.rm = TRUE)
+
+# even more duplicate checking
+length(unique(pcin_1$id)) == nrow(pcin_wide)
+
+
+# how many occasions (ss) per session (pp)
+sess_obs <- sally %>%
+  group_by(pp) %>%
+  dplyr::select(pp, ss) %>%
+  summarise(K = max(ss, na.rm = TRUE)) %>%
+  arrange(pp)
+
+sess_obs_1 <- pcin_1 %>%
+  group_by(pp) %>%
+  dplyr::select(pp, ss) %>%
+  summarise(K = max(ss, na.rm = TRUE)) %>%
+  arrange(pp)
+
+sess_obs == sess_obs_1
+
+# Check sex for consistency by mark (this is the problem for plot 1)
+
+
+#-------- Set up oSCR data ---------
 
 pcin_1_oscr <- data2oscr(edf = pcin_1,
                          sess.col = 1,
@@ -82,6 +152,7 @@ plot(pcin_1_oscr$scrFrame, jit = 2)
 pcin_1_ss <- make.ssDF(scrFrame = pcin_1_oscr$scrFrame, buffer = 4, res=0.5)
 plot(ssDF = pcin_1_ss, scrFrame = pcin_1_oscr$scrFrame)
 
+#---------- SCR Analysis ---------
 
 m16 <- oSCR.fit(model = list(D ~ pp, #density
                              p0 ~ pp + sex, #detection
